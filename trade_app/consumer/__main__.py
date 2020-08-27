cb9829fae88ff4a53e403f2f2a78c59bd04cf09d
Andres Plazas <acpr87@gmail.com>
2020-08-27 14:31:30 +0200

    Test receiving messages using an independent threat

--- trade_app/consumer/__main__.py
+++ trade_app/consumer/__main__.py
@@ -1,6 +1,16 @@
+import time
+
 from .app import Consumer
 import sys
+import threading
 
 if __name__ == '__main__':
+    read_task_event = threading.Event()
     consumer = Consumer(sys.argv[1])
-    consumer.star_read()
+
+    thread_one = threading.Thread(name='Blocking read Threat', target=consumer.star_read)
+    thread_one.start()
+
+    while True:
+        print('Message from main threat')
+        time.sleep(3)
